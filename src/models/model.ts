d3782772e4d78fe8b241f20d3bd33214205d524f
diogo <a94877@uminho.pt>
2025-03-10 14:11:11 +0000

    BD model

---
+++ src/models/model.ts
@@ -0,0 +1,45 @@
+import mongoose, { Document, Schema, model, models } from 'mongoose';
+
+//----------------------------------
+
+export interface IMessage {
+    role: "user" | "bot";
+    text: string;
+}
+
+export interface IConversation {
+    messages: IMessage[];
+    thumbnail?: string;
+    created_at: Date;
+    extra_info?: any; 
+}
+
+export interface IUser extends Document {
+    _username: string;
+    conversations: IConversation[];
+}
+
+//----------------------------------
+
+const MessageSchema = new Schema<IMessage>({
+    role: { type: String, required: true, enum: ["user", "bot"] },
+    text: { type: String, required: true },
+});
+
+const ConversationSchema = new Schema<IConversation>({
+    messages: [MessageSchema],
+    thumbnail: { type: String },
+    created_at: { type: Date, default: Date.now },
+    extra_info: Schema.Types.Mixed,
+});
+
+const UserSchema = new Schema<IUser>({
+    _username: { type: String, required: true, unique: true },
+    conversations: [ConversationSchema],
+
+});
+
+const User = models.User || model<IUser>('User', UserSchema);
+
+export default User;
+
