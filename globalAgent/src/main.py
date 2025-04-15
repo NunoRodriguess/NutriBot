b6ac4d0b1438d4af60061516e1167af83a001386
= <luistborges@outlook.com>
2025-04-15 11:15:39 +0100

    fixes bugs + added validation

--- globalAgent/src/main.py
+++ globalAgent/src/main.py
@@ -1,5 +1,6 @@
-from typing import List, Dict
 from LLMClient import LLMClient
+import pandas as pd
+import time
 
 def create_classification_prompt(agents, question):
         # List all available topics with descriptions
@@ -17,10 +18,14 @@ Available agents:
 Instructions:
 1. Analyze the question carefully
 2. Determine which agent topic best matches the question
-3. Return ONLY the topic ID (e.g., "medical" or "sleep") without any explanation
-4. If the question doesn't clearly match any topic, return "general"
+3. This step is extremely important: Your answer needs to be a SINGLE word. You can ONLY return one of the following options, without any explanation!
+- nutrition
+- supplements
+- exercise
+- habits
+- monitoring
 
-Question: {question}
+Question: {question} 
 Topic:"""
         
         return prompt
@@ -45,12 +50,14 @@ def main():
                 "name": "Habits Agent",
                 "description": "Handles questions about sleep, work/leisure hours, ergonomics, hygiene, smoking, etc"
             },
-            "monotoring": {
+            "monitoring": {
                 "name": "Monitoring Agent",
                 "description": "Handles medical monoroting and questions about preventive medicine"
             },
             
         }
+    
+    # Input version
     
     while True:
         question = input("\nAsk a question (or 'EXIT' to quit): ")
@@ -62,6 +69,31 @@ def main():
         agent = response.strip().lower()
 
         print(f"Agent: {agent}")
+    
+    
+    # Test version
+    '''
+    test_file = pd.read_csv('test/test_questions.csv')
+    correct_count = 0
+    for i,r in test_file.iterrows():
+        print('Question being analyzed')
+        time.sleep(1)
+        question = r['question']
+        agent = r['agent']
+
+        q = create_classification_prompt(agents, question)
+        response = llm_client.generateResponse(q)
+        chosen_agent = response.strip().lower()
+
+        if chosen_agent == agent:
+             correct_count += 1
+        else:
+             print(f"Failed on question: {question}\nCorrect agent: {agent} || Chosen Agent: {chosen_agent}")
+
+    print(f"Correct agent picked {correct_count} out of 60 times!")
+    # 55/60
+    '''
+
 
 if __name__ == "__main__":
     main()
